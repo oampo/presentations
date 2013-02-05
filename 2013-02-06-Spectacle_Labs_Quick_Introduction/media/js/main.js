d80aa883e6d15d4eb95b60be5d2942e4d44c0c15
Joe Turner <joe@oampo.co.uk>
2013-02-05 15:50:40 +0000

    Add quick introduction

---
+++ 2013-02-06-Spectacle_Labs_Quick_Introduction/media/js/main.js
@@ -0,0 +1,61 @@
+var Presentation = function() {
+    this.slides = $('section');
+    this.index = 0;
+
+    this.disableContextMenu();
+
+    $(document).keydown(this.onKeyDown.bind(this));
+    $(document).mousedown(this.onMouseClick.bind(this));
+
+    this.move();
+};
+
+Presentation.prototype.move = function() {
+    this.slides.filter(":visible").fadeOut("fast", function() {
+        $(this.slides[this.index]).fadeIn("fast");
+    }.bind(this));
+};
+
+Presentation.prototype.next = function() {
+    if (this.index == this.slides.length - 1) {
+        return;
+    }
+    this.index += 1;
+    this.move();
+};
+
+Presentation.prototype.prev = function() {
+    if (this.index == 0) {
+        return;
+    }
+    this.index -= 1;
+    this.move();
+};
+
+Presentation.prototype.onKeyDown = function(event) {
+    if (event.keyCode == 37) {
+        this.prev();
+    }
+    else if (event.keyCode == 32 || event.keyCode == 39) {
+        this.next();
+    }
+};
+
+Presentation.prototype.onMouseClick = function(event) {
+    if (event.which == 1) {
+        this.next();
+    }
+    else if (event.which == 3) {
+        this.prev();
+    }
+};
+
+Presentation.prototype.disableContextMenu = function() {
+    $(document).bind("contextmenu", function(event) {
+        return false;
+    });
+};
+
+$(document).ready(function() {
+    var presentation = new Presentation();
+})
